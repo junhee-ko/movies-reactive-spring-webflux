b4a1e3216ade43ef0b5e94a3f41b5fb885022803
junhee-ko <junheee.ko@gmail.com>
2021-12-31 00:12:33 +0900

    Add FluxAndMonoControllerTest three other approach tests

---
+++ movies-service/src/test/kotlin/unit/jko/moviesservice/controller/FluxAndMonoControllerTest.kt
@@ -0,0 +1,55 @@
+package jko.moviesservice.controller
+
+import org.junit.jupiter.api.Test
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.test.StepVerifier
+
+@WebFluxTest(controllers = [FluxAndMonoController::class])
+@AutoConfigureWebTestClient
+internal class FluxAndMonoControllerTest(
+    @Autowired private val webTestClient: WebTestClient
+) {
+
+    @Test
+    fun flux_1() {
+        webTestClient.get()
+            .uri("/flux")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+            .expectBodyList(Int::class.java)
+            .hasSize(3)
+    }
+
+    @Test
+    fun flux_2() {
+        val flux = webTestClient.get()
+            .uri("/flux")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+            .returnResult(Int::class.java)
+            .responseBody
+
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3)
+            .verifyComplete()
+    }
+
+    @Test
+    fun flux_3() {
+        webTestClient.get()
+            .uri("/flux")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+            .expectBodyList(Int::class.java)
+            .consumeWith<WebTestClient.ListBodySpec<Int>> {
+                assert(it.responseBody?.size == 3)
+            }
+    }
+}
