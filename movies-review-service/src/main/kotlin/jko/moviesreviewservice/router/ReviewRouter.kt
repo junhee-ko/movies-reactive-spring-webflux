bfbe994cb485a438cc5b302061422ca8b3ae0070
junhee-ko <junheee.ko@gmail.com>
2022-01-11 00:24:38 +0900

    Update nesting endpoints

--- movies-review-service/src/main/kotlin/jko/moviesreviewservice/router/ReviewRouter.kt
+++ movies-review-service/src/main/kotlin/jko/moviesreviewservice/router/ReviewRouter.kt
@@ -3,6 +3,7 @@ package jko.moviesreviewservice.router
 import jko.moviesreviewservice.handler.ReviewHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RequestPredicates.path
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -13,9 +14,12 @@ class ReviewRouter {
     @Bean
     fun reviewsRoute(reviewHandler: ReviewHandler): RouterFunction<ServerResponse> {
         return route()
+            .nest(path("/v1/reviews")) { builder ->
+                builder
+                    .POST("") { request -> reviewHandler.addReview(request) }
+                    .GET("") { request -> reviewHandler.getReviews(request) }
+            }
             .GET("/v1/helloworld") { ServerResponse.ok().bodyValue("helloworld") }
-            .POST("/v1/reviews") { request -> reviewHandler.addReview(request) }
-            .GET("/v1/reviews") { request -> reviewHandler.getReviews(request) }
             .build()
     }
 }
