eb74d579b3dbff0aa5b225327d535eb8ff5c429d
junhee-ko <junheee.ko@gmail.com>
2022-01-12 01:06:32 +0900

    Add WebClientConfig

---
+++ movies-service/src/main/kotlin/jko/moviesservice/config/WebClientConfig.kt
@@ -0,0 +1,14 @@
+package jko.moviesservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig {
+
+    @Bean
+    fun webClient(builder: WebClient.Builder): WebClient {
+        return builder.build()
+    }
+}
