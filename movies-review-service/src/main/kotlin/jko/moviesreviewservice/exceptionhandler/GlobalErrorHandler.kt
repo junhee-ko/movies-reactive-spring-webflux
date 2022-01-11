f093da77e06e377b7b3b6b95f531e6cd5e926cd5
junhee-ko <junheee.ko@gmail.com>
2022-01-12 00:18:07 +0900

    Add GlobalErrorHandler

---
+++ movies-review-service/src/main/kotlin/jko/moviesreviewservice/exceptionhandler/GlobalErrorHandler.kt
@@ -0,0 +1,33 @@
+package jko.moviesreviewservice.exceptionhandler
+
+import jko.moviesreviewservice.exception.ReviewDataException
+import org.slf4j.LoggerFactory
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferFactory
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class GlobalErrorHandler : ErrorWebExceptionHandler {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun handle(exchange: ServerWebExchange, ex: Throwable): Mono<Void> {
+        logger.error("Exception message: {}", ex.message, ex)
+
+        val bufferFactory: DataBufferFactory = exchange.response.bufferFactory()
+        val errorMessage: DataBuffer = bufferFactory.wrap(ex.message!!.toByteArray())
+
+        if (ex is ReviewDataException) {
+            exchange.response.statusCode = HttpStatus.BAD_REQUEST
+
+            return exchange.response.writeWith(Mono.just(errorMessage))
+        }
+
+        exchange.response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+        return exchange.response.writeWith(Mono.just(errorMessage))
+    }
+}
