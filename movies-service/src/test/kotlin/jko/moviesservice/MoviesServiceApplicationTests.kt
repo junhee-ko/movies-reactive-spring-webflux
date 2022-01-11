e3230f03979ada5cf141fb87b0d94f0760dbe6b0
junhee-ko <junheee.ko@gmail.com>
2022-01-12 00:44:02 +0900

    Add movies-service module

---
+++ movies-service/src/test/kotlin/jko/moviesservice/MoviesServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.moviesservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
